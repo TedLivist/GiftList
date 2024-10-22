5331df9908b3a3ba0cf678798fbaa2dd0e22c8d2
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2024-10-22 12:57:27 +0100

    Implemented solution

--- client/index.js
+++ client/index.js
@@ -7,8 +7,16 @@ const serverUrl = 'http://localhost:1225';
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const merkle = new MerkleTree(niceList)
+
+  let name = niceList[0]
+  let nameIndex = niceList.findIndex((a) => a === name)
+  let proof = merkle.getProof(nameIndex)
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: proof,
+    name: name
   });
 
   console.log({ gift });
